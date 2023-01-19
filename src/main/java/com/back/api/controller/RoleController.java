672cb34186b0b20d48b54a47d728c871053358ad
Paul Odon <stg-eni-paul-odon@livenexx.net>
2023-01-19 14:48:20 +0330

    add gttps

--- src/main/java/com/back/api/controller/RoleController.java
+++ src/main/java/com/back/api/controller/RoleController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin("http://localhost:3000/")
+@CrossOrigin("https://localhost:3000/")
 public class RoleController {
 
     @Autowired
